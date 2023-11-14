19af9870b5360a1d59d9611555b5915d7cba01f8
Rosoner <rosoner@abv.bg>
2023-11-14 20:37:45 +0200

    Add Game Create page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import GameList from './components/game-list/GameList';
-// import GameCreate from './components/game-create/GameCreate';
+import GameCreate from './components/game-create/GameCreate';
 // import Login from './components/login/Login';
 // import Register from './components/register/Register';
 // import GameDetails from './components/game-details/GameDetails';
@@ -16,8 +16,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/games" element={<GameList />} />
-                {/* <Route path="/games/create" element={<GameCreate />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/games/create" element={<GameCreate />} />
+                {/* <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/games/:gameId" element={<GameDetails /> } /> */}
             </Routes>
