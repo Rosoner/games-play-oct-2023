534adf3f0cd0e412e45de1314ec6672b03a316c8
Rosoner <rosoner@abv.bg>
2023-11-14 20:31:54 +0200

    Add GameList page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
-// import GameList from './components/game-list/GameList';
+import GameList from './components/game-list/GameList';
 // import GameCreate from './components/game-create/GameCreate';
 // import Login from './components/login/Login';
 // import Register from './components/register/Register';
@@ -15,8 +15,8 @@ function App() {
 
             <Routes>
                 <Route path="/" element={<Home />} />
-                {/* <Route path="/games" element={<GameList />} />
-                <Route path="/games/create" element={<GameCreate />} />
+                <Route path="/games" element={<GameList />} />
+                {/* <Route path="/games/create" element={<GameCreate />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/games/:gameId" element={<GameDetails /> } /> */}
