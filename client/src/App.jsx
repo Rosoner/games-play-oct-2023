17e55b5aa8c42d58614ff27e4b6a029d14340709
Rosoner <rosoner@abv.bg>
2023-11-15 08:39:24 +0200

    add services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import GameList from './components/game-list/GameList';
 import GameCreate from './components/game-create/GameCreate';
-// import Login from './components/login/Login';
-// import Register from './components/register/Register';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
 // import GameDetails from './components/game-details/GameDetails';
 
 function App() {
@@ -17,9 +17,9 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/games" element={<GameList />} />
                 <Route path="/games/create" element={<GameCreate />} />
-                {/* <Route path="/login" element={<Login />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/games/:gameId" element={<GameDetails /> } /> */}
+                {/* <Route path="/games/:gameId" element={<GameDetails /> } /> */}
             </Routes>
         </div>
     )
--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,29 +1,28 @@
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-// import * as gameService from '../../services/gameService';
+import * as gameService from '../../services/gameService';
 
 export default function GameCreate() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     
-    // const createGameSubmitHandler = async (e) => {
-    //     e.preventDefault();
+    const createGameSubmitHandler = async (e) => {
+        e.preventDefault();
 
-    //     const gameData = Object.fromEntries(new FormData(e.currentTarget));
+        const gameData = Object.fromEntries(new FormData(e.currentTarget));
 
-    //     try {
-    //         await gameService.create(gameData);
+        try {
+            await gameService.create(gameData);
 
-    //         navigate('/games');
-    //     } catch (err) {
-    //         // Error notification
-    //         console.log(err);
-    //     }
-    // }
+            navigate('/games');
+        } catch (err) {
+            // Error notification
+            console.log(err);
+        }
+    }
 
     return (
         <section id="create-page" className="auth">
-            {/* <form id="create" onSubmit={createGameSubmitHandler}> */}
-            <form id="create">
+            <form id="create" onSubmit={createGameSubmitHandler}>
                 <div className="container">
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
